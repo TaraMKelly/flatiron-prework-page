84d0f96025af59f778564a94d3d21c83dfb8213a
Tara Kelly <tara.kelly16@gmail.com>
2021-05-27 17:50:34 -0400

    added paragraph element

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
+h2.textContent = "About Me!"; 
 document.querySelector('body').appendChild(h2); 
+
+const p = document.createElement('p');
+p.textContent = "Welcome to the world of Tara Kelly!"
+document.querySelector('body').appendChild(p);
+
