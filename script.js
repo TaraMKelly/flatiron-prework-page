92f2d406b7bd4114830d499331ff6f6891ad58b2
Tara Kelly <tara.kelly16@gmail.com>
2021-06-19 13:16:02 -0400

    added bio in html

--- script.js
+++
@@ -1,8 +0,0 @@
-const h2 = document.createElement('h2');
-h2.textContent = "About Me!"; 
-document.querySelector('body').appendChild(h2); 
-
-const p = document.createElement('p');
-p.textContent = "Welcome to the world of Tara Kelly!"
-document.querySelector('body').appendChild(p);
-
